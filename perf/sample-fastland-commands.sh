b78ab08e72c932e83676192f6c0e91bfed4224cf
Vinh Nguyen <vinh.nguyen@v-key.com>
2022-07-03 10:53:11 +0700

    Add changeBuildGradleContent method to change build.gradle file content

---
+++ perf/sample-fastland-commands.sh
@@ -0,0 +1,5 @@
+# Change minSdk
+fastlane changeBuildGradleContent file:app/build.gradle existingKey:minSdkVersion modifiedValue:21
+
+# Change versionName
+fastlane changeBuildGradleContent file:app/build.gradle existingKey:versionName modifiedValue:\"3.0\"
